45f44aec33e4ebc6154a47a5d206eed3c94d105c
chen shu ting <a.0066715@gmail.com>
2021-11-13 23:34:31 +0800

    修改tasks視圖

--- resources/views/layouts/tasks.blade.php
+++ resources/views/layouts/tasks.blade.php
@@ -33,4 +33,18 @@
     </div>
 
     <!-- 代辦：目前任務 -->
+    @if (count($tasks) > 0)
+        <tbody>
+        @foreach ($tasks as $task)
+            <tr>
+                <!-- 任務名稱 -->
+                <td class="table-text">
+                    <div>{{ $task->name }} </div>
+                </td>
+                <td>
+                    <!-- 代辦：刪除按鈕 -->
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
 @endsection
