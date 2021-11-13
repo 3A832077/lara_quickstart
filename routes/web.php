4afe89da4a834ed1a879bca1acce76b2f01431df
chen shu ting <a.0066715@gmail.com>
2021-11-13 23:30:23 +0800

    用以新增任務 ，含驗證輸入內容

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,17 @@ Route::get('/', function () {
 
 // 增加新的任務
 Route::post('/task', function (Request $request) {
-    //
+    // 驗證輸入
+    $validator = Validator::make($request->all() , [
+        'name' => 'required|max:255',
+    ]);
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+    // 建立該任務...
+//新增任務存入DB的程式碼 (see next page)
 });
 // 刪除任務
 Route::delete('/task/{task}', function (Task $task) {
